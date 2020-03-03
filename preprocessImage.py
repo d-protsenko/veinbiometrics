4129519120b798d25a4bf3891e3b19887fdc3b5a
Danil Protsenko <dan.protsenko@gmail.com>
2020-03-04 02:04:23 +0600

    WIP

--- preprocessImage.py
+++ preprocessImage.py
@@ -4,6 +4,7 @@ import cv2
 
 defaultCLAHEValue = 4
 
+
 def parse_arguments():
     parser = ArgumentParser(description='Preprocess image')
     parser.add_argument('-in', '--input',
@@ -34,15 +35,24 @@ def main():
     img = cv2.imread(args.input_image)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.fastNlMeansDenoising(gray, gray, 4)
+    mask = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.threshold(gray, thresh=240, maxval=255, type=cv2.THRESH_BINARY, dst=mask)
 
+    # im_thresh_gray = cv2.bitwise_and(gray, mask)
+    # mask3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR) #back to 3 channel img -> bitwise_and(...)
+
+    cv2.imwrite('mask.png', mask)
+    cv2.imwrite('mask_inverted.png', cv2.bitwise_not(mask))
     final = multi_clahe(gray, defaultCLAHEValue)
 
     cv2.imwrite(args.output_image, final)
+
     # cv2.imshow('image', final)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
+    # bash preprocess.sh ./from_db/orig/101_3_f2_1.bmp finger_test.png
 
 
 if __name__ == '__main__':
     main()
-
